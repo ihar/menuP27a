edb3bf44e73baeb0620461d3dfa8ff7c5939b964
ihar <ihar.safonau@gmail.com>
2019-01-22 22:22:15 +0300

    Extract menu dates as datetime.date values

--- helpers/menu_extractor.py
+++ helpers/menu_extractor.py
@@ -1,5 +1,7 @@
 import sys
 import pandas as pd
+import re
+from datetime import datetime
 
 from pathlib import Path
 
@@ -13,12 +15,40 @@ class MenuExtractor:
         self.path = path
         self._excel_data = None
         self._menu_keys = []
+        self._menu_dates = []
         try:
             self._excel_data = pd.read_excel(str(self.path), sheet_name=None)
+            # The menu's title is in the header: self._excel_data.columns[0]
         except FileNotFoundError as err:
             raise err
         # self._excel_data is an OrderedDict
         self._menu_keys = list(self._excel_data.keys())
+        self._menu_dates = self._extract_menu_dates()
+
+    def _preprocess_menu(self, df_menu):
+        """
+        Drop NA columns that exist because menus in Excel files contain merged cells.
+        The merged cells cause extra columns in pandas data frames having NA values.
+        Those columns' names look like "Unnamed: 1"
+        :param df_menu: pandas data frame as result of the pd.read_excel function
+        :return: pandas data frame without NA columns and values of the mereged cells moved
+        to additional column's value
+        """
+        df_menu_processed = df_menu.dropna(axis=1, how='all')
+        # In case there is empty rows
+        df_menu_processed = df_menu_processed.dropna(axis=0, how='all')
+
+    def _extract_menu_dates(self):
+        menu_dates = []
+        for menu_df in self._excel_data.values():
+            menu_title = menu_df.columns[0]
+            date_str = re.findall(r"\d{2}\.\d{2}\.\d{4}", menu_title)
+            try:
+                date_date = datetime.strptime(date_str[0], "%d.%m.%Y").date()
+            except (TypeError, ValueError):
+                date_date = ''
+            menu_dates.append(date_date)
+        return menu_dates
 
     @property
     def excel_menus(self):
@@ -27,3 +57,7 @@ class MenuExtractor:
     @property
     def menu_keys(self):
         return list(self._menu_keys)
+
+    @property
+    def menu_dates(self):
+        return self._menu_dates
--- tests/test_excel_parse.py
+++ tests/test_excel_parse.py
@@ -1,5 +1,6 @@
 import pytest
 from pathlib import Path
+import datetime
 
 from helpers.menu_extractor import MenuExtractor
 
@@ -26,3 +27,17 @@ def test_get_menu_keys(file_path, menu_keys):
     mex = MenuExtractor(Path(file_path))
     assert mex.menu_keys == menu_keys
 
+
+# menu_keys is a list of tab names of an Excel file;
+# usually, that names are dates in string format
+@pytest.mark.parametrize("file_path,menu_dates", [
+    ("data/Меню с 02.04 по 06.04.2018г.xls",
+        [datetime.date(2018, 4, 2), datetime.date(2018, 4, 3), datetime.date(2018, 4, 4),
+         datetime.date(2018, 4, 5), datetime.date(2018, 3, 30)]),  # the last one is unexpected, by the way
+    ("data/Меню с 7-11 май.xls",
+     [datetime.date(2018, 5, 7), datetime.date(2018, 5, 8),
+      datetime.date(2018, 5, 10), datetime.date(2018, 5, 11)]),
+])
+def test_get_menu_dates(file_path, menu_dates):
+    mex = MenuExtractor(Path(file_path))
+    assert mex.menu_dates == menu_dates
