438bdc9618e66aff0ded02b3b8bc0432b78824d2
ihar <ihar.safonau@gmail.com>
2019-01-23 21:09:56 +0300

    Extract menu data as a dictionary in the menu_key => menu_df format
    menu_df data frames are formatted a bit

--- helpers/menu_extractor.py
+++ helpers/menu_extractor.py
@@ -16,6 +16,7 @@ class MenuExtractor:
         self._excel_data = None
         self._menu_keys = []
         self._menu_dates = []
+        self._menu_dic = {}
         try:
             self._excel_data = pd.read_excel(str(self.path), sheet_name=None)
             # The menu's title is in the header: self._excel_data.columns[0]
@@ -24,8 +25,12 @@ class MenuExtractor:
         # self._excel_data is an OrderedDict
         self._menu_keys = list(self._excel_data.keys())
         self._menu_dates = self._extract_menu_dates()
+        # Preprocess each tab of the excel file and generate a dictionary
+        # in the menu_key => menu_df dictionary
+        self._menu_dic = {menu_key: self._preprocess_menu(self._excel_data[menu_key]) for menu_key in self._menu_keys}
 
-    def _preprocess_menu(self, df_menu):
+    @staticmethod
+    def _preprocess_menu(df_menu):
         """
         Drop NA columns that exist because menus in Excel files contain merged cells.
         The merged cells cause extra columns in pandas data frames having NA values.
@@ -37,8 +42,27 @@ class MenuExtractor:
         df_menu_processed = df_menu.dropna(axis=1, how='all')
         # In case there is empty rows
         df_menu_processed = df_menu_processed.dropna(axis=0, how='all')
+        df_menu_processed.columns = ['Food', 'Weight', 'Price']
+        # Rows with NA in the last column is the names of a section like
+        # "Супы", "Гарниры", "Сладкие блюда" and so on.
+        # Delete these rows and put the sections' names as values in the additional columns
+        fourth_column_values = []
+        current_type = ''
+        for row in df_menu_processed.itertuples(index=True, name='Pandas'):
+            if pd.isnull(row.Price):
+                current_type = row.Food
+            fourth_column_values.append(current_type)
+        df_menu_processed['Type'] = fourth_column_values
+        # Delete the rows with the sections' names:
+        df_menu_processed = df_menu_processed.dropna(axis=0, subset=['Price'])
+        return df_menu_processed
 
     def _extract_menu_dates(self):
+        """
+        Convert tab names which are in the dd.mm.yyyy format
+        to a date object
+        :return: A list of dates that correspond to the tab names
+        """
         menu_dates = []
         for menu_df in self._excel_data.values():
             menu_title = menu_df.columns[0]
@@ -61,3 +85,7 @@ class MenuExtractor:
     @property
     def menu_dates(self):
         return self._menu_dates
+
+    @property
+    def menu_data_dic(self):
+        return self._menu_dic
