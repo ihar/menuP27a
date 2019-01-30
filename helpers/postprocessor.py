92219af87b2683cc15f47bfa78f14c0e8ea52243
ihar <ihar.safonau@gmail.com>
2019-01-30 20:53:31 +0300

    Add a function that converts value from the Price column (string) into its numeric representation.

--- helpers/postprocessor.py
+++ helpers/postprocessor.py
@@ -7,6 +7,23 @@ def price_to_number(price):
     :param price: string that indicates price of an item in a menu; "1 руб 82 коп", "70 коп" and similar
     :return: a number; "1 руб 82 коп" -> 1.82, "70 коп" -> 0.7 or None value if the input is in unknown format
     """
-    re_rub = r'(\d+)\s*?руб.*'
-    re_kop = r'(\d+)\s*?коп.*'
-    return None
+    re_rub = re.compile(r'(\d+)\s*?руб.*', re.IGNORECASE)
+    re_kop = re.compile(r'(\d+)\s*?коп.*', re.IGNORECASE)
+    rub = re_rub.findall(price)
+    kop = re_kop.findall(price)
+    try:
+        rub_num = int(rub[0])
+    except IndexError:
+        rub_num = None
+    try:
+        kop_num = int(kop[0])
+    except IndexError:
+        kop_num = None
+    if rub_num is not None and kop_num is not None:
+        return rub_num + kop_num/100
+    elif rub_num is not None:
+        return rub_num
+    elif kop_num is not None:
+        return kop_num/100
+    else:
+        return None
