9a0c9983d4a8621bfce2ba05d968625ecbe6a8e9
ihar <ihar.safonau@gmail.com>
2019-02-07 22:02:30 +0300

    Rename output csv file
    There will be another csv file with data suitable for visualizations. Script processed all menu files available for now.

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from glob import glob
 from helpers.menu_extractor import MenuExtractor
 
 input_path = './data'
-output_file = './data/collected_menu_data.csv'
+output_file = './data/raw_menu_data.csv'
 
 if __name__ == '__main__':
     input_files = glob(os.path.join(input_path, '*.xls*'))
     print("Files to process: %d" % len(input_files))
     with open(output_file, 'w',  encoding='utf-8') as f:
         for fpath in input_files:
+            print("\t%s" %fpath)
             mex = MenuExtractor(fpath)
             menu_data = mex.menus_combined
             menu_data['File'] = os.path.basename(fpath)
