dd0e5e9eca45ee545f79cced73ed53a9d9880f8c
ihar <ihar.safonau@gmail.com>
2019-11-26 21:01:52 +0300

    Add more tests for Yandex speller

--- tests/test_yandex_speller.py
+++ tests/test_yandex_speller.py
@@ -2,14 +2,20 @@ import pytest
 from helpers.yandex_speller import YandexSpeller
 
 
-def test_correct_word():
+@pytest.mark.parametrize("word",
+                         ["салат", "Салат", "пудинг", "чесночной", "бендерики", "капрезе"])
+def test_correct_word(word):
     speller = YandexSpeller()
-    suggestions = [response['s'][0] for response in speller.spell('корова')]
+    suggestions = [response['s'][0] for response in speller.spell(word)]
     assert 0 == len(suggestions)
 
 
-def test_incorrect_word():
+@pytest.mark.parametrize("word,correct_word",
+                         [("салад", "салат"), ("пуддинг", "пудинг"),
+                          ("тушенная", "тушеная"), ("пшеная", "пшенная"),
+                          ("пшонная", "пшенная"), ("пшрнная", "пшенная")])
+def test_incorrect_word(word, correct_word):
     speller = YandexSpeller()
-    suggestions = [response['s'][0] for response in speller.spell('карова')]
-    assert 1 == len(suggestions) and 'корова' == suggestions[0]
+    suggestions = [response['s'][0] for response in speller.spell(word)]
+    assert 1 == len(suggestions) and correct_word == suggestions[0]
 
